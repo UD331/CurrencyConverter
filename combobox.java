a0a8ff27c7c8ade405f34f36eb2cba0213654699
thanhle <88635563+thanhinhchtom@users.noreply.github.com>
2021-09-18 17:58:30 -0500

    Update combobox.java

--- combobox.java
+++ combobox.java
@@ -4,23 +4,33 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class combobox extends JFrame{
-    JComboBox<String> cbox1;
-    JComboBox<String> cbox2;
+    double total = 0;
+    String[] currencies = {"Choose currency"};
+    public static JComboBox<String> cbox1;
+    public static JComboBox<String> cbox2;
     JButton exchange;
     JButton reset;
+    JButton add1;
     JLabel amount = new JLabel("Amount of money want to convert: ");
     JLabel amount1 = new JLabel("Amount of money after converted: ");
+    JLabel bill = new JLabel("CALCULATE THE WHOLE BILL");
 
     combobox(){
-
-
         //create frame
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
         Font font = new Font(null,Font.BOLD,20);
         JTextField numbertochange1 = new JTextField();
+        JTextField convertednum = new JTextField();
         numbertochange1.setBounds(400,100,100,30);
+        convertednum.setBounds(400, 200, 100,30);
+        convertednum.setFont(font);
         numbertochange1.setFont(font);
         this.add(numbertochange1);
+
+        //bill text
+        bill.setBounds(250, 10, 500, 30);
+        bill.setFont(font);
+        this.add(bill);
 
         //first text
         amount.setBounds(10,100,350,30);
@@ -33,12 +43,11 @@ public class combobox extends JFrame{
         this.add(amount1);
 
         // create the first combo box
-        String[] currencies = {"Choose currency","USD", "Euro", "Pound"};
         cbox1 = new JComboBox<>(currencies);
         Rectangle r = new Rectangle(200,30);
         cbox1.setBounds(r);
         cbox1.setLocation(550,100);
-        this.add(cbox1);
+        cbox1.setFocusable(false);
         cbox1.addActionListener(e -> {
             if(e.getSource()==cbox1){
                 String currencynow = (String) cbox1.getSelectedItem();
@@ -50,8 +59,9 @@ public class combobox extends JFrame{
 
         // create the second combo box
         cbox2 = new JComboBox<>(currencies);
-        this.add(cbox2);
+
         cbox2.setBounds(550,200,200,30);
+        cbox2.setFocusable(false);
         cbox2.addActionListener(e -> {
             if(e.getSource()==cbox2){
                 String currencynow = (String) cbox2.getSelectedItem();
@@ -60,6 +70,15 @@ public class combobox extends JFrame{
                 }
             }
         });
+        addcurrencies ad = new addcurrencies();
+        try {
+            ad.additem();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.add(cbox1);
+        this.add(cbox2);
 
         // create exchange button
         exchange = new JButton("Exchange");
@@ -72,8 +91,25 @@ public class combobox extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource()==exchange) {
-                    float money = Float.parseFloat(numbertochange1.getText());
+                    double money = Double.parseDouble(numbertochange1.getText());
                     System.out.printf("%.2f \n", money);
+                }
+            }
+        });
+
+        // create exchange button
+        add1 = new JButton("Add");
+        add1.setBounds(200,300,100,50);
+        add1.setFocusable(false);
+        this.add(add1);
+        //catch event when click button
+        add1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource()==add1) {
+                    double money = Double.parseDouble(numbertochange1.getText());
+                    total += money;
+                    System.out.printf("%.2f \n", total);
                 }
             }
         });
@@ -89,6 +125,8 @@ public class combobox extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource()==reset) {
                     numbertochange1.setText("");
+                    cbox1.setSelectedIndex(0);
+                    cbox2.setSelectedIndex(0);
                 }
             }
         });
@@ -98,3 +136,4 @@ public class combobox extends JFrame{
         this.setVisible(true);
     }
 }
+
